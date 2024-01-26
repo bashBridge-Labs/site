6eb0b62b6342eab83f7a1a0f769307af96962ed1
Dibakar <dibakarbera01@gmail.com>
2024-01-26 23:35:36 +0530

    contact page

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -3,13 +3,24 @@ import React from 'react'
 export default function page() {
   return (
     <>
-    <div className='w-full h-[100vh]'>
-        
+      <div class="	font-family:ui-serif, mb-5 text-4xl  text-center dark:text-black">
+        Connect With Us
+      </div>
+      <div class="mb-3 text-3xl text-center dark:text-black">
+        <a href="mailto: bashbridgelabs@gmail.com">
+          Email :- bashbridgelabs@gmail.com
+        </a>
+      </div>
+      <div class="mb-3 text-2xl text-center dark:text-black">
+        We would be happy to hear from you, please fill in the form below or mail us your requirements on
+      </div>
+      <div className='w-full h-[100vh]'>
+
         <div data-tf-live="01HN2F5DC5TG8C2W65SDCSXFEH"></div><script src="//embed.typeform.com/next/embed.js"></script>
-        
-    </div>
+
+      </div>
     </>
-   
-    
+
+
   )
 }
