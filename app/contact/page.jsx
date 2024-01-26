f5df077fee01c6cf96a671ad5db0638f5a7f742b
Dibakar <dibakarbera01@gmail.com>
2024-01-27 00:05:31 +0530

    contact page

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,77 +1,26 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 
-export default function Page() {
-  useEffect(() => {
-    const loadScript = async () => {
-      const script = document.createElement('script')
-      script.src = '//embed.typeform.com/next/embed.js'
-      script.async = true
-      document.body.appendChild(script)
-    }
-
-    loadScript()
-  }, [])
-
+export default function page() {
   return (
     <>
-      <div className="font-family:ui-serif, mb-5 text-4xl text-center dark:text-black">
+      <div class="	font-family:ui-serif, mb-5 text-4xl  text-center dark:text-black">
         Connect With Us
       </div>
-      <div className="mb-3 text-3xl text-center dark:text-black">
+      <div class="mb-3 text-3xl text-center dark:text-black">
         <a href="mailto: bashbridgelabs@gmail.com">
           Email :- bashbridgelabs@gmail.com
         </a>
       </div>
-      <div className="mb-3 text-2xl text-center dark:text-black">
+      <div class="mb-3 text-2xl text-center dark:text-black">
         We would be happy to hear from you, please fill in the form below or mail us your requirements on
       </div>
-      <div className='w-full h-[100vh]'>
-        <div data-tf-live="01HN2F5DC5TG8C2W65SDCSXFEH"></div>
-      </div>
+      {/* <div className='w-full h-[100vh]'>
+
+        <div data-tf-live="01HN2F5DC5TG8C2W65SDCSXFEH"></div><script src="//embed.typeform.com/next/embed.js"></script>
+
+      </div> */}
     </>
+
+
   )
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React from 'react'
-
-// export default function page() {
-//   return (
-//     <>
-//       <div class="	font-family:ui-serif, mb-5 text-4xl  text-center dark:text-black">
-//         Connect With Us
-//       </div>
-//       <div class="mb-3 text-3xl text-center dark:text-black">
-//         <a href="mailto: bashbridgelabs@gmail.com">
-//           Email :- bashbridgelabs@gmail.com
-//         </a>
-//       </div>
-//       <div class="mb-3 text-2xl text-center dark:text-black">
-//         We would be happy to hear from you, please fill in the form below or mail us your requirements on
-//       </div>
-//       <div className='w-full h-[100vh]'>
-
-//         <div data-tf-live="01HN2F5DC5TG8C2W65SDCSXFEH"></div><script src="//embed.typeform.com/next/embed.js"></script>
-
-//       </div>
-//     </>
-
-
-//   )
-// }
