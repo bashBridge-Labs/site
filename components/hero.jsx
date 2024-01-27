19c672a44068cb01353c9411ee8123a73b650a18
Dibakar <dibakarbera01@gmail.com>
2024-01-28 00:32:08 +0530

    contact update

--- components/hero.jsx
+++ components/hero.jsx
@@ -18,7 +18,7 @@ export default function Hero() {
               </div>
               <div className="flex flex-col items-center justify-center gap-3 mt-10 lg:flex-row lg:justify-start">
                 <a
-                  href="#_"
+                  href="/contact"
                   className="items-center justify-center w-full px-6 py-2.5 text-center text-white bg-[#4860FF] hover:scale-105 duration-200 hover:bg-zinc-600   rounded-full inline-flex  lg:w-auto border-2 border-stone-200 "
                 >
                   {jsonData.heroButton1}
